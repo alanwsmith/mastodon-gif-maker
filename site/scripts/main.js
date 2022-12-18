2483cc07089387f5d188b3f9db382589d534d1a5
Alan Smith <commit@alanwsmith.com>
2022-12-18 14:48:15 -0500

    Got the text lines in place

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -3,8 +3,18 @@ const s = {
     loaded: 0,
 }
 
+const els = {}
+const elNames = ['gifCanvas', 'line1', 'line2', 'line3', 'line4', 'line5']
+
 const runIt = () => {
-    console.log('runit')
+    console.log('Creating GIF')
+
+    s.gif = new GIF({
+        workers: 2,
+        quality: 10,
+        workerScript: 'scripts/gif.worker.js',
+    })
+
     s.images.forEach((image) => {
         s.gif.addFrame(image, { delay: 60 })
     })
@@ -13,30 +23,59 @@ const runIt = () => {
         window.open(URL.createObjectURL(blob))
     })
 
-    s.gif.render()
+    // s.gif.render()
 }
 
 const checkIt = () => {
     s.loaded++
     if (s.loaded === s.images.length) {
         runIt()
+        handleInput()
     }
 }
 
+const handleInput = () => {
+    s.context.clearRect(0, 0, 480, 320)
+    s.context.drawImage(s.images[0], 0, 0)
+    s.context.font = 'bold 24px Arial'
+    s.context.fillStyle = `rgba(200, 200, 200, 0.8)`
+    s.context.fillRect(0, 250, 480, 100)
+    s.context.fillStyle = 'black'
+    s.context.fillText(els['line1'].value, 20, 24)
+    s.context.fillText(els['line2'].value, 20, 48)
+    s.context.fillText(els['line3'].value, 20, 72)
+    s.context.fillText(els['line4'].value, 20, 272)
+    s.context.font = 'bold 18px Arial'
+    s.context.fillText(els['line5'].value, 20, 302)
+
+    // s.context.strokeStyle = 'white'
+    // s.context.strokeText(els['line1'].value, 20, 24)
+    // s.context.strokeText(els['line2'].value, 20, 48)
+    // s.context.strokeText(els['line3'].value, 20, 72)
+    // s.context.strokeText(els['line4'].value, 20, 272)
+    // s.context.strokeText(els['line5'].value, 20, 302)
+}
+
 const init = () => {
-    console.log('Initializing')
+    console.log(`Initializing: ${new Date().getTime()}`)
+
+    elNames.forEach((el) => {
+        els[el] = document.getElementById(el)
+    })
+
+    els['line1'].addEventListener('input', handleInput)
+    els['line2'].addEventListener('input', handleInput)
+    els['line3'].addEventListener('input', handleInput)
+    els['line4'].addEventListener('input', handleInput)
+    els['line5'].addEventListener('input', handleInput)
+
+    s.context = els['gifCanvas'].getContext('2d')
 
     for (let i = 0; i < 60; i++) {
         s.images.push(new Image())
         s.images[i].src = `frames/claiton-conto-11489108/${i + 1}.jpg`
         s.images[i].addEventListener('load', checkIt)
     }
-
-    s.gif = new GIF({
-        workers: 2,
-        quality: 10,
-        workerScript: 'scripts/gif.worker.js',
-    })
 }
 
 document.addEventListener('DOMContentLoaded', init)
